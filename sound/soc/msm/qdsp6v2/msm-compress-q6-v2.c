7c91234018f6a8d703dd8e3f93ea67a0771de557
Preetam Singh Ranawat <apranawat@codeaurora.org>
2016-06-27 16:54:07 +0530

    ASoC: open ASM session with 24 bit for 24 bit playback
    For 24 bit playback decoder is opened with 16 bits per sample, So
    decoder output is 16 bit only.
    Set bits per sample to 24 if codec format is SNDRV_PCM_FORMAT_S24_3LE.
    Set bits per sample to 16 as default value in capture prepare use case.
    
    CRs-Fixed: 1035154
    Change-Id: I14271b17441308f8ec6dfaea566c01887d1233f4
    Signed-off-by: Preetam Singh Ranawat <apranawat@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -942,10 +942,19 @@ static int msm_compr_configure_dsp(struct snd_compr_stream *cstream)
 	};
 
 	pr_debug("%s: stream_id %d\n", __func__, ac->stream_id);
-	if (prtd->codec_param.codec.format == SNDRV_PCM_FORMAT_S24_LE)
+
+	if ((prtd->codec_param.codec.format == SNDRV_PCM_FORMAT_S24_LE) ||
+		(prtd->codec_param.codec.format == SNDRV_PCM_FORMAT_S24_3LE))
 		bits_per_sample = 24;
 	else if (prtd->codec_param.codec.format == SNDRV_PCM_FORMAT_S32_LE)
 		bits_per_sample = 32;
+
+    //use 24bits to get rid of 16bits innate noise
+    //mark by globale value to open adm 24bits
+    if (prtd->codec_param.codec.bit_rate == 24) {
+        bits_per_sample = 24;
+        gis_24bits = 1;
+    }
 
 	if (prtd->compr_passthr != LEGACY_PCM) {
 		ret = q6asm_open_write_compressed(ac, prtd->codec,
